fd426a7fda4908667aad58f65f0e83cb04c53c19
Kevin Clement <kevin.clement@gmail.com>
2019-08-07 17:39:01 -0700

    working serial audio

--- src/logic.cpp
+++ src/logic.cpp
@@ -1,4 +1,5 @@
 #include "Arduino.h"
+#include <HardwareSerial.h>
 #include "EEPROM.h"
 #include "logic.h"
 #include "consts.h"
@@ -24,10 +25,15 @@ int resolution = 8;
 #define LS_ONE_PIN A2
 #define LS_TWO_PIN A3
 
+HardwareSerial MySerial(1);
+static int8_t Send_buf[8] = {0};
+
 Logic::Logic() 
   : serial(*this)
 {
 }
+
+void sendCommand(int8_t command, int16_t dat);
 
 void Logic::setup() {
   serial.setup();
@@ -47,6 +53,29 @@ void Logic::setup() {
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(STBY, OUTPUT);
+
+  MySerial.begin(9600, SERIAL_8N1, 16, 17);
+  delay(500);              //Wait chip initialization is complete
+  sendCommand(0X09, 0X02); // select the TF card
+  delay(200);
+  sendCommand(0X22, 0X4601);
+}
+
+void sendCommand(int8_t command, int16_t dat)
+{
+  delay(20);
+  Send_buf[0] = 0x7e; //starting byte
+  Send_buf[1] = 0xff; //version
+  Send_buf[2] = 0x06; //the number of bytes of the command without starting byte and ending byte
+  Send_buf[3] = command; //
+  Send_buf[4] = 0x00;//0x00 = no feedback, 0x01 = feedback
+  Send_buf[5] = (int8_t)(dat >> 8);//datah
+  Send_buf[6] = (int8_t)(dat); //datal
+  Send_buf[7] = 0xef; //ending byte
+  for(uint8_t i=0; i<8; i++)//
+  {
+    MySerial.write(Send_buf[i]) ;
+  }
 }
 
 void Logic::handle() {
