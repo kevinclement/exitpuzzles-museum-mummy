874236979c156b0027937949aa4b9d1b213392e0
Kevin Clement <kevin.clement@gmail.com>
2019-08-10 16:23:48 -0700

    move sound logic

--- src/logic.cpp
+++ src/logic.cpp
@@ -1,13 +1,6 @@
 #include "Arduino.h"
-#include <HardwareSerial.h>
-#include "EEPROM.h"
 #include "logic.h"
 #include "consts.h"
-
-int FOO_VAR_ADDR = 0;         // where to store foo in eeprom
-
-HardwareSerial MySerial(1);
-static int8_t Send_buf[8] = {0};
 
 Logic::Logic() 
   : serial(*this),
@@ -17,8 +10,6 @@ Logic::Logic()
     sound(*this)
 {
 }
-
-void sendCommand(int8_t command, int16_t dat);
 
 void Logic::setup() {
   serial.setup();
@@ -31,30 +22,8 @@ void Logic::setup() {
 
   serial.printHelp();
   printVariables();
-
-  MySerial.begin(9600, SERIAL_8N1, 16, 17);
-  delay(500);              //Wait chip initialization is complete
-  sendCommand(0X09, 0X02); // select the TF card
-  delay(200);
-  sendCommand(0X22, 0X4601);
 }
 
-void sendCommand(int8_t command, int16_t dat)
-{
-  delay(20);
-  Send_buf[0] = 0x7e; //starting byte
-  Send_buf[1] = 0xff; //version
-  Send_buf[2] = 0x06; //the number of bytes of the command without starting byte and ending byte
-  Send_buf[3] = command; //
-  Send_buf[4] = 0x00;//0x00 = no feedback, 0x01 = feedback
-  Send_buf[5] = (int8_t)(dat >> 8);//datah
-  Send_buf[6] = (int8_t)(dat); //datal
-  Send_buf[7] = 0xef; //ending byte
-  for(uint8_t i=0; i<8; i++)//
-  {
-    MySerial.write(Send_buf[i]);
-  }
-}
 
 void Logic::handle() {
   serial.handle();
@@ -64,7 +33,7 @@ void Logic::handle() {
 }
 
 void Logic::readStoredVariables() {
-  EEPROM.begin(64); // don't need a big size
+  //EEPROM.begin(64); // don't need a big size
   //EEPROM.get(FOO_VAR_ADDR, FOO_VAR);
 }
 
--- src/sound.cpp
+++ src/sound.cpp
@@ -1,5 +1,11 @@
 #include "Arduino.h"
 #include "sound.h"
+#include <HardwareSerial.h>
+
+void sendCommand(int8_t command, int16_t dat);
+
+HardwareSerial MySerial(1);
+static int8_t Send_buf[8] = {0};
 
 Sound::Sound(Logic &logic)
 : _logic(logic)
@@ -7,4 +13,29 @@ Sound::Sound(Logic &logic)
 }
 
 void Sound::setup() {
+  MySerial.begin(9600, SERIAL_8N1, 16, 17);
+  delay(500);              //Wait chip initialization is complete
+  sendCommand(0X09, 0X02); // select the TF card
+  delay(200);
+  sendCommand(0X22, 0X4601);
 }
+
+void sendCommand(int8_t command, int16_t dat)
+{
+  delay(20);
+  Send_buf[0] = 0x7e; //starting byte
+  Send_buf[1] = 0xff; //version
+  Send_buf[2] = 0x06; //the number of bytes of the command without starting byte and ending byte
+  Send_buf[3] = command; //
+  Send_buf[4] = 0x00;//0x00 = no feedback, 0x01 = feedback
+  Send_buf[5] = (int8_t)(dat >> 8);//datah
+  Send_buf[6] = (int8_t)(dat); //datal
+  Send_buf[7] = 0xef; //ending byte
+  for(uint8_t i=0; i<8; i++)//
+  {
+    MySerial.write(Send_buf[i]);
+  }
+}
+
+
+
